9f7d18335d5c8d01692ba3fbbc727fae255d2f1b
muharremdemirel <91542346+muharremdemirel@users.noreply.github.com>
2023-05-15 14:39:21 +0300

    Add files via upload

---
+++ Hafta-1/VKI.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class VKI {
+    public static void main(String[] args){
+
+        double kilo,boy;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Boyunuzu metre cinsinden giriniz: ");
+        boy = inp.nextDouble();
+
+        System.out.print("Kilonuzu giriniz: ");
+        kilo = inp.nextDouble();
+
+        double vkı = kilo / (boy * boy);
+        System.out.print("Vücut Kütle İndeksiniz: " + vkı);
+
+
+
+    }
+    
+}
