81161e0eebd5b78c690f1c34ea3bd45232fdb637
muharremdemirel <91542346+muharremdemirel@users.noreply.github.com>
2023-05-15 03:54:27 +0300

    Add files via upload

---
+++ Hafta-1/DaireAlanı.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class DaireAlanı {
+    public static void main(String[] args){
+        double pi = 3.14,r,a,alan;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Yarıçap değerini giriniz: ");
+        r = inp.nextDouble();
+        System.out.print("Merkez açının ölçüsünü giriniz: ");
+        a = inp.nextDouble();
+        alan = (pi * (r*r) * a) / 360;
+        System.out.print("Dairenin alan: " + alan);
+
+    }
+    
+}
