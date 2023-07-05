9deccc4191876dd46a649b284e5a75a5d0ecd29d
Muharrem Demirel <91542346+muharremdemirel@users.noreply.github.com>
2023-07-05 08:35:17 +0300

    Add files via upload

---
+++ Hafta-2/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static boolean isPalindrom(int numara){
+        int tmp = numara, yeniNumara=0, sonSayi;
+        while(tmp != 0){
+
+            sonSayi = tmp % 10;
+            yeniNumara = (yeniNumara * 10) + sonSayi;
+            tmp/=10;
+        }
+
+        if (numara ==yeniNumara)
+            return true;
+        else 
+            return false;
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(3547));
+        
+        
+    }
+    
+}
